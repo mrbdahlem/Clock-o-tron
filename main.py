87f50fdf3c9cb4a686148651954ac660b863c6f9
Brian Dahlem <dahlem.brian@cusd80.com>
2023-08-29 20:25:08 -0700

    commenting & cleaning

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ PRESSED = 0
 ON_COLOR = (64, 128, 0)
 OFF_COLOR = (4, 0, 4)
 
+# set up hardware connections
 min_btn = Pin(0, mode=Pin.IN, pull=Pin.PULL_UP)
 hour_btn = Pin(1, mode=Pin.IN, pull=Pin.PULL_UP)
 sqw = Pin(2, mode=Pin.IN, pull=None)
 face_pin = Pin(13, mode=Pin.OUT, value=0)
 
+# set up the neopixel clock display
+face = Display(face_pin, ON_COLOR, OFF_COLOR)
+
+# set up the hardware real-time clock
+i2c = I2C(1, sda=Pin(14), scl=Pin(15))
+ds = DS3231(i2c)
+ds.square_wave(freq=ds.FREQ_1024) # Enable 1024 Hz output from RTC
+
+# set up square wave input IRQ handler to manage display updates
 trigger = 0
 def set_trigger(pin):
     global trigger
@@ -20,18 +30,9 @@ def set_trigger(pin):
 
 sqw.irq(handler=set_trigger, trigger=Pin.IRQ_FALLING)
 
-face = Display(face_pin, ON_COLOR, OFF_COLOR)
-
-i2c = I2C(1, sda=Pin(14), scl=Pin(15))
-
-ds = DS3231(i2c)
-ds.square_wave(freq=ds.FREQ_1024) # Enable 1024 Hz output from RTC
-
-rtc = machine.RTC()
-
 now = ds.datetime()
 
-
+# quickly test display
 face.show(0, 0, 63)
 time.sleep(1)
 face.show(0, 63, 0)
@@ -39,7 +40,7 @@ time.sleep(1)
 face.show(15, 0, 0)
 time.sleep(1)
 
-# Set a default date and time
+# Set a default date and time if hardware rtc has no data (new or lost battery)
 if now[0] < 2023:   # type: ignore
     year = 2023 # Can be yyyy or yy format
     month = 8
@@ -54,22 +55,25 @@ if now[0] < 2023:   # type: ignore
     now = ds.datetime()
     print("time reset")
 
+# Setup micropython's rtc and set it from the hardware rtc
+rtc = machine.RTC()
 rtc.datetime(now)
-
 print(rtc.datetime())
 
+# get the current date and time to initialize variables
 (year, month, mday, weekday, hour, minute, sec, _) = ds.datetime() # type: ignore
 
 trigger = 0
 hour_updated = False
 minute_updated = False
-
 while True:
-    if trigger >= 34: #update at ~30Hz
+    # update the display at ~30Hz at most
+    if trigger >= 34:
         (year, month, mday, weekday, hour, minute, sec, _) = ds.datetime() # type: ignore
         face.show(hour, minute, sec)
         trigger = 0
     
+    # Check for button presses to set the time
     if min_btn.value() == PRESSED:
         if not minute_updated:
             minute = (minute + 1) % 60
@@ -84,8 +88,12 @@ while True:
     else:
         hour_updated = False
 
+    # if the user updated the time, set the hardware RTC and show the new time
     if minute_updated or hour_updated:
         ds.datetime((year, month, mday, hour, minute, 0))
         face.show(hour, minute, sec)
+        now = ds.datetime()
+        rtc.datetime(now)
+        print(rtc.datetime())
     
     time.sleep_ms(100)
